bc145dbf465a3fb1665808630b80ae11990bfbf5
PoppinL <poppinlp@gmail.com>
2015-03-18 16:42:27 +0800

    update tooltip

--- src/js/doggy/_tooltip.js
+++ src/js/doggy/_tooltip.js
@@ -4,64 +4,106 @@
  * @param {Object} config
  * @param {String} config.position tooltip相对于按钮定位的位置
  */
-doggy.initTooltip = function (selContainer, config) {
-    var ndContainer = $(selContainer);
-    if (!ndContainer || ndContainer.data('init')) return;
 
-    var _config = {
-        position: 'tc'
-    };
+'use strict';
+
+doggy.initTooltip = function (selContainer, config) {
+    var $ = _rq.$,
+        _config = {
+            position: 'tc',
+            content: '',
+            autoHide: true
+        },
+        $container = $(selContainer),
+        hideFlag = false,
+        tooltip = new Tooltip(),
+        cbEnter,
+        cbLeave;
+
+    if (!$container || $container.data('init')) return;
+
     $.extend(_config, config);
 
-    ndContainer
-        .on('mouseenter', function () {
-            var tooltip = new doggy.Tooltip();
-
-            tooltip.content(ndContainer.data('tooltip'));
-            tooltip.render(ndContainer, _config.position);
-            tooltip.show();
-        })
-        .on('mouseleave', function () {
-            var tooltip = new doggy.Tooltip();
+    if (_config.autoHide) {
+        cbEnter = function () {
+            tooltip.content(_config.content).render($container, _config.position).show();
+        };
+        cbLeave = function () {
             tooltip.hide();
-        })
-        .data('init', true);
+        };
+    } else {
+        cbEnter = function () {
+            if (hideFlag) {
+                hideFlag = false;
+            }
+            tooltip.content(_config.content).render($container, _config.position).show();
+        };
+        cbLeave = function () {
+            hideFlag = true;
+            setTimeout(function () {
+                if (hideFlag) {
+                    tooltip.hide();
+                }
+            }, 200);
+        };
+        tooltip.$tooltip.bind('mouseenter', function () {
+            hideFlag = false;
+        });
+        tooltip.$tooltip.bind('mouseleave', cbLeave);
+    }
+
+    $container.bind('mouseenter', cbEnter).bind('mouseleave', cbLeave).data('init', true);
 };
 
 // Tooltip类
 doggy.Tooltip = function () {
-    this.ndTooltip = $('.J-tooltip');
-    if (!this.ndTooltip.length) {
-        this.ndTooltip = $([
-            '<span class="J-tooltip tooltip">',
-                '<span class="tooltip__content"></span>',
-                '<i class="tooltip__arrow"></i>',
-                '<i class="tooltip__arrow tooltip__arrow--mask"></i>',
-            '</span>'
-        ].join(''));
-        $('body').append(this.ndTooltip);
-    }
+    var that = this;
+
+    that.$tooltip = _rq.$('.js-tooltip');
+
+    if (that.$tooltip.length) return;
+
+    that.$tooltip = _rq.$([
+        '<div class="js-tooltip tooltip">',
+        '<span class="js-tooltip-content"></span>',
+        '<i class="tooltip-arrow"></i>',
+        '<i class="tooltip-arrow tooltip-arrow-mask"></i>',
+        '</div>'
+    ].join(''));
+
+    that.$tooltip.delegate('.js-tooltip-close', 'click', function () {
+        that.hide.call(that);
+    });
+
+    _rq.$(document.body).append(that.$tooltip);
 };
 
 doggy.Tooltip.prototype = {
-    constructor: doggy.Tooltip,
+    constructor: Tooltip,
     show: function () {
-        this.ndTooltip.addClass('tooltip--active');
+        this.$tooltip.addClass('tooltip-active');
+        return this;
     },
     hide: function () {
-        this.ndTooltip.removeClass('tooltip--active');
+        this.$tooltip.removeClass('tooltip-active');
+        return this;
     },
-    render: function (ndTarget, position) {
-        this.ndTooltip.removeClass().addClass('J-tooltip tooltip tooltip--' + position);
-        doggy.initPosition({
-            selSelf: this.ndTooltip,
-            selTarget: ndTarget,
+    render: function (selTarget, position) {
+        var that = this;
+
+        that.$tooltip.removeClass().addClass('js-tooltip tooltip tooltip-' + position);
+        _rq.widget.initPosition({
+            selSelf: that.$tooltip,
+            selTarget: selTarget,
             position: position,
             offset: 10
         });
+
+        return that;
     },
     content: function (content) {
-        this.ndTooltip.children('.tooltip__content').html(content);
+        this.$tooltip.children('.js-tooltip-content').html(content);
+        return this;
     }
 };
 
